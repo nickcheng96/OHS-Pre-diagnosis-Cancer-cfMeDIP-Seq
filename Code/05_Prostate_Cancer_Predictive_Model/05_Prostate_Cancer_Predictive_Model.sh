ca7193736e0edacd70a4274765d85f452bc353b7
nickcheng96 <76527949+nickcheng96@users.noreply.github.com>
2026-01-14 04:07:36 -0500

    Add files via upload

--- Code/05_Prostate_Cancer_Predictive_Model/05_Prostate_Cancer_Predictive_Model.sh
+++ Code/05_Prostate_Cancer_Predictive_Model/05_Prostate_Cancer_Predictive_Model.sh
@@ -2,11 +2,11 @@
 for i in {1..10}; #10 folds
 do for l in {1..100}; #100 repeats
 do sed  s/foldno/${i}/g prostate.cancer.discovery.cv.model.R  | sed  s/seedno/${l}/g   > prostate.cancer.discovery.cv.${i}.${l}.R;
-./prostate.cancer.discovery.cv.${i}.${l}.R;
+RScript ./prostate.cancer.discovery.cv.${i}.${l}.R;
 done; done
 
 #2 100-fold CV performance discovery
 ./prostate.cancer.discovery.cv.performance.R
 
-#test set model + performance
+#3 test set model + performance
 ./prostate.cancer.testset.R
